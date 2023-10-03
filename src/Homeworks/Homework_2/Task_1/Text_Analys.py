8d1cf15c7ae5360df61eb46d894d1de2d7844ff6
Engelsgeduld <knyazev.dmitrii05@gmail.com>
2023-10-03 19:51:14 +0300

    changes were made

--- src/Homeworks/Homework_2/Task_1/Text_Analys.py
+++ src/Homeworks/Homework_2/Task_1/Text_Analys.py
@@ -3,42 +3,39 @@ import csv
 
 
 def words_counter(file, counted_words):
-    with open(file) as F:
-        for line in F:
-            """Не стал оборачивать в list comprehension. Теряется логика кода и выражение излишне длинное"""
-            for word in set(line.split()):
-                if word in counted_words:
-                    counted_words[word] += line.count(word)
-                else:
-                    counted_words[word] = line.count(word)
+    with open(file) as opened_file:
+        for line in opened_file:
+            for word in line.split():
+                counted_words[word] = counted_words.get(word, 0) + 1
     return counted_words
 
 
 def write_to_csv(file, counted_words):
-    with open(file, "w") as F:
-        write = csv.writer(F, delimiter=":")
+    with open(file, "w") as opened_file:
+        write = csv.writer(opened_file, delimiter=":")
         write.writerows([word, str(counted_words[word])] for word in counted_words)
 
-    with open(file) as f:
-        print(f.read())
 
-
-def input_check(user_input):
-    user_input = user_input.split()
+def input_check(*args):
+    user_input = args
     if len(user_input) != 2:
-        print("Не верный ввод файлов")
-        exit()
+        print("Не верное количество файлов")
+        return False
     src, drs = user_input
     if exists(src) and exists(drs):
-        return src, drs
+        return True
     else:
         print("Указанные файлы не найдены")
-        exit()
+        return False
 
 
 def main():
     counted_words = {}
-    src, drs = input_check(input("Введите через пробел файлы \n"))
+    user_input = input("Введите через пробел файлы \n").split()
+    while not input_check(*user_input):
+        user_input = input("Введите через пробел файлы \n").split()
+    else:
+        src, drs = user_input
     counted_words = words_counter(src, counted_words)
     write_to_csv(drs, counted_words)
 
