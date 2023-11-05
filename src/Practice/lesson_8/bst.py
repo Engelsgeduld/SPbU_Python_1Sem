55278fb8e012cdb2dd7cfc7175e1a74e939b3bb8
Engelsgeduld <knyazev.dmitrii05@gmail.com>
2023-11-05 11:43:22 +0300

    Changes: mapp -> map, Node field key and data not Optional, remove class Pair -> Tuple, remove cringe, remove Data_type, V -> Value

--- src/Practice/lesson_8/bst.py
+++ src/Practice/lesson_8/bst.py
@@ -1,30 +1,23 @@
 from dataclasses import dataclass
 from typing import Generic, TypeVar, Optional, Callable, Iterable
 
-V = TypeVar("V")
+Value = TypeVar("Value")
 Key = TypeVar("Key")
-Data_type = TypeVar("Data_type")
 
 
 @dataclass
-class Node(Generic[Key, V]):
-    key: Optional[Key]
-    data: Optional[V]
-    right_children: Optional["Node[V]"] = None
-    left_children: Optional["Node[V]"] = None
-
-
-@dataclass
-class Pair:
-    first: Node[Key, V]
-    second: Node[Key, V]
+class Node(Generic[Key, Value]):
+    key: Key
+    data: Value
+    right_children: Optional["Node[Value]"] = None
+    left_children: Optional["Node[Value]"] = None
 
 
 @dataclass
 class Tree:
     size: int = 0
-    root: Optional[Node[Key, V]] = None
-    type: Optional[Data_type] = None
+    root: Optional[Node[Key, Value]] = None
+    type: type = int
 
 
 def _valid_input_key(map: Tree, key: Key) -> bool:
@@ -35,28 +28,28 @@ def create_tree_map() -> Tree:
     return Tree()
 
 
-def _extract_max(root: Node[Key, V]) -> Pair:
+def _extract_max(root: Node[Key, Value]) -> tuple:
     if root.right_children is not None:
-        pair = _extract_max(root.right_children)
-        root.right_children = pair.second
-        return Pair(pair.first, root)
-    return Pair(root, root.left_children)
+        pair = (_extract_max(root.right_children),)
+        root.right_children = pair[1]
+        return pair[0], root
+    return root, root.left_children
 
 
-def delete_tree_map(mapp: Tree) -> None:
-    def delete_recursion(cur_node: Node[V, Key]):
+def delete_tree_map(map: Tree) -> None:
+    def delete_recursion(cur_node: Node[Value, Key]):
         if cur_node.left_children is not None:
             delete_recursion(cur_node.left_children)
         if cur_node.right_children is not None:
             delete_recursion(cur_node.right_children)
 
         del cur_node
-        mapp.size -= 1
+        map.size -= 1
 
-    delete_recursion(mapp.root)
+    delete_recursion(map.root)
 
 
-def _find(root: Node[V, Key], key: Key) -> Node[V, Key] | None:
+def _find(root: Node[Value, Key], key: Key) -> Node[Value, Key] | None:
     if root is None:
         return None
     if key > root.key:
@@ -67,19 +60,17 @@ def _find(root: Node[V, Key], key: Key) -> Node[V, Key] | None:
 
 
 def has_key(map: Tree, key) -> bool:
-    if _find(map.root, key) is not None:
-        return True
-    return False
+    return _find(map.root, key) is not None
 
 
-def get(map: Tree, key) -> V:
+def get(map: Tree, key) -> Value:
     maybe_node = _find(map.root, key)
     if maybe_node is None:
         raise ValueError("Key not in BST")
     return maybe_node.data
 
 
-def put(map: Tree, key: Key, value: V) -> ():
+def put(map: Tree, key: Key, value: Value) -> ():
     if map.type is not None and not _valid_input_key(map, key):
         raise ValueError("Keys must be one type")
     _insert(map.root, key, value) if map.size != 0 else _insert_main_root(
@@ -88,7 +79,7 @@ def put(map: Tree, key: Key, value: V) -> ():
     map.size += 1
 
 
-def _insert_main_root(map: Tree, key: Key, value: V):
+def _insert_main_root(map: Tree, key: Key, value: Value):
     map.root = Node(key, value)
     if key is not None:
         map.type = type(key)
@@ -96,7 +87,7 @@ def _insert_main_root(map: Tree, key: Key, value: V):
         raise ValueError("Key can not be None")
 
 
-def _insert(root: Node[V, Key], key: Key, value: V) -> Node[V, Key]:
+def _insert(root: Node[Value, Key], key: Key, value: Value) -> Node[Value, Key]:
     if root is None:
         return Node(key, value)
     if root.key > key:
@@ -106,27 +97,27 @@ def _insert(root: Node[V, Key], key: Key, value: V) -> Node[V, Key]:
     return root
 
 
-def remove(map: Tree, key: Key) -> V:
+def remove(map: Tree, key: Key) -> Value:
     old_value = get(map, key)
     _delete(map.root, key)
     map.size -= 1
     return old_value
 
 
-def _create_new_root(root: Node[V, Key]) -> Node[V, Key]:
+def _create_new_root(root: Node[Value, Key]) -> Node[Value, Key]:
     if root.left_children is None:
         new_root = root.right_children
     elif root.right_children is None:
         new_root = root.left_children
     else:
         pair = _extract_max(root.left_children)
-        new_root = pair.first
-        new_root.left_children = pair.second
+        new_root = pair[0]
+        new_root.left_children = pair[1]
         new_root.right_children = root.right_children
     return new_root
 
 
-def _delete(root: Node, key: Key) -> Node[V, Key]:
+def _delete(root: Node, key: Key) -> Node[Value, Key]:
     if root is None:
         raise ValueError("This key not exist")
     if root.key > key:
@@ -139,22 +130,22 @@ def _delete(root: Node, key: Key) -> Node[V, Key]:
     return root
 
 
-def _preorder_comparator(node: Node[V, Key]) -> Iterable[Node[V, Key]]:
+def _preorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
     return filter(None, (node, node.left_children, node.right_children))
 
 
-def _inorder_comparator(node: Node[V, Key]) -> Iterable[Node[V, Key]]:
+def _inorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
     return filter(None, (node.left_children, node, node.right_children))
 
 
-def _postorder_comparator(node: Node[V, Key]) -> Iterable[Node[V, Key]]:
+def _postorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
     return filter(None, (node.left_children, node.right_children, node))
 
 
-def traverse(map: Tree, order: str) -> list[V]:
+def traverse(map: Tree, order: str) -> list[Value]:
     nodes = []
 
-    def traverse_recursion(cur_node: Node[V, Key], order_func: Callable):
+    def traverse_recursion(cur_node: Node[Value, Key], order_func: Callable):
         node_order = order_func(cur_node)
         for node in node_order:
             if node is not cur_node:
