c224909c7253ca60c8fbb351b4b73478b868f1de
Knyazev Dmitrii <144917566+Engelsgeduld@users.noreply.github.com>
2023-11-07 20:04:17 +0300

    Practice 8 Task 1 BST Knyazev (#18)
    * Init: Practice 8 Task 1 BST Knyazev
    
    * Changes: mapp -> map, Node field key and data not Optional, remove class Pair -> Tuple, remove cringe, remove Data_type, V -> Value
    
    * Change: class Tree add Generic values, class Node add Value to r/l children, code refactoring, None key check moved to _valid_input_key

---
+++ Tests/Practice/lesson_8_bst/test_bst.py
@@ -0,0 +1,119 @@
+from src.Practice.lesson_8.bst import *
+import pytest
+
+from src.Practice.lesson_8.bst import _valid_input_key, _insert_main_root
+
+
+@pytest.fixture()
+def empy_tree_initialization():
+    empty_tree = create_tree_map()
+    return empty_tree
+
+
+@pytest.fixture()
+def tree_initialization_with_values():
+    keys = [5, 3, 4, 6, 8, 9, 2, 7]
+    values = list(map(str, keys))
+    tree = create_tree_map()
+    for i in range(len(keys)):
+        put(tree, keys[i], values[i])
+    return tree
+
+
+def test_creation_tree():
+    actual = create_tree_map()
+    assert actual == Tree(0)
+
+
+@pytest.mark.parametrize(
+    "tree_type, key, result", ((int, 8, True), (str, 8, False), (None, None, False))
+)
+def test_valid_input_key(tree_type, key, result, empy_tree_initialization):
+    empy_tree_initialization.type = tree_type
+    assert _valid_input_key(empy_tree_initialization, key) is result
+
+
+@pytest.mark.parametrize(
+    "key, tree_type", ((8, str), ([1, 2], tuple), ({2, 3}, list), (1, bool))
+)
+def test_put_exception(key, tree_type, empy_tree_initialization):
+    empy_tree_initialization.type = tree_type
+    with pytest.raises(ValueError):
+        put(empy_tree_initialization, key, 1)
+
+
+@pytest.mark.parametrize(
+    "key, values", ((5, "5"), (3, "3"), (4, "4"), (6, "6"), (8, "8"), (9, "9"))
+)
+def test_get_function(key, values, tree_initialization_with_values):
+    actual = get(tree_initialization_with_values, key)
+    assert actual == values
+
+
+@pytest.mark.parametrize(
+    "key, result",
+    (
+        (5, True),
+        (3, True),
+        (4, True),
+        (6, True),
+        (8, True),
+        (9, True),
+        (25, False),
+        (1024, False),
+    ),
+)
+def test_has_key(key, result, tree_initialization_with_values):
+    actual = has_key(tree_initialization_with_values, key)
+    assert actual == result
+
+
+def test_get_exception(tree_initialization_with_values):
+    with pytest.raises(ValueError):
+        get(tree_initialization_with_values, 12)
+
+
+def test_insert_main_root(empy_tree_initialization):
+    _insert_main_root(empy_tree_initialization, 8, 12)
+    assert empy_tree_initialization.root == Node(8, 12)
+
+
+def test_insert_main_root_exception(empy_tree_initialization):
+    with pytest.raises(ValueError):
+        _insert_main_root(empy_tree_initialization, None, [1, 2])
+
+
+@pytest.mark.parametrize(
+    "key, order",
+    [
+        (8, ["2", "4", "3", "9", "7", "6", "5"]),
+        (3, ["4", "2", "7", "9", "8", "6", "5"]),
+    ],
+)
+def test_remove(tree_initialization_with_values, key, order):
+    print(traverse(tree_initialization_with_values, "post-order"))
+    remove(tree_initialization_with_values, key)
+    print(tree_initialization_with_values)
+    actual = traverse(tree_initialization_with_values, "post-order")
+    assert actual == order
+
+
+@pytest.mark.parametrize("key, value", [(8, "8"), (3, "3")])
+def test_remove_value(tree_initialization_with_values, key, value):
+    actual = remove(tree_initialization_with_values, key)
+    assert actual == value
+
+
+def test_preorder(tree_initialization_with_values):
+    actual = traverse(tree_initialization_with_values, "pre-order")
+    assert actual == ["5", "3", "2", "4", "6", "8", "7", "9"]
+
+
+def test_inorder(tree_initialization_with_values):
+    actual = traverse(tree_initialization_with_values, "in-order")
+    assert actual == ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def test_postorder(tree_initialization_with_values):
+    actual = traverse(tree_initialization_with_values, "post-order")
+    assert actual == ["2", "4", "3", "7", "9", "8", "6", "5"]
---
+++ src/Practice/lesson_8/bst.py
@@ -0,0 +1,164 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Optional, Callable, Iterable
+
+Value = TypeVar("Value")
+Key = TypeVar("Key")
+
+
+@dataclass
+class Node(Generic[Key, Value]):
+    key: Key
+    data: Value
+    right_children: Optional["Node[Key ,Value]"] = None
+    left_children: Optional["Node[Key ,Value]"] = None
+
+
+@dataclass
+class Tree(Generic[Key, Value]):
+    size: int = 0
+    root: Optional[Node[Key, Value]] = None
+    type: type = int
+
+
+def _valid_input_key(map: Tree, key: Key) -> bool:
+    return key is not None if map.type is None else type(key) == map.type
+
+
+def create_tree_map() -> Tree:
+    return Tree()
+
+
+def _extract_max(root: Node[Key, Value]) -> tuple:
+    if root.right_children is not None:
+        pair = (_extract_max(root.right_children),)
+        root.right_children = pair[1]
+        return pair[0], root
+    return root, root.left_children
+
+
+def delete_tree_map(map: Tree) -> None:
+    def delete_recursion(cur_node: Node[Value, Key]):
+        if cur_node.left_children is not None:
+            delete_recursion(cur_node.left_children)
+        if cur_node.right_children is not None:
+            delete_recursion(cur_node.right_children)
+
+        del cur_node
+        map.size -= 1
+
+    delete_recursion(map.root)
+
+
+def _find(root: Node[Value, Key], key: Key) -> Node[Value, Key] | None:
+    if root is None:
+        return None
+    if key > root.key:
+        return _find(root.right_children, key)
+    elif key < root.key:
+        return _find(root.left_children, key)
+    return root
+
+
+def has_key(map: Tree, key) -> bool:
+    return _find(map.root, key) is not None
+
+
+def get(map: Tree, key) -> Value:
+    maybe_node = _find(map.root, key)
+    if maybe_node is None:
+        raise ValueError("Key not in BST")
+    return maybe_node.data
+
+
+def put(map: Tree, key: Key, value: Value) -> ():
+    if map.type is not None and not _valid_input_key(map, key):
+        raise ValueError("Keys must be one type")
+    if map.size != 0:
+        _insert(map.root, key, value)
+    else:
+        _insert_main_root(map, key, value)
+    map.size += 1
+
+
+def _insert_main_root(map: Tree, key: Key, value: Value):
+    if _valid_input_key(map, key):
+        map.root = Node(key, value)
+    else:
+        raise ValueError("Key can not be None")
+
+
+def _insert(root: Node[Value, Key], key: Key, value: Value) -> Node[Value, Key]:
+    if root is None:
+        return Node(key, value)
+    if root.key > key:
+        root.left_children = _insert(root.left_children, key, value)
+    elif root.key < key:
+        root.right_children = _insert(root.right_children, key, value)
+    return root
+
+
+def remove(map: Tree, key: Key) -> Value:
+    old_value = get(map, key)
+    _delete(map.root, key)
+    map.size -= 1
+    return old_value
+
+
+def _create_new_root(root: Node[Value, Key]) -> Node[Value, Key]:
+    if root.left_children is None:
+        new_root = root.right_children
+    elif root.right_children is None:
+        new_root = root.left_children
+    else:
+        pair = _extract_max(root.left_children)
+        new_root = pair[0]
+        new_root.left_children = pair[1]
+        new_root.right_children = root.right_children
+    return new_root
+
+
+def _delete(root: Node, key: Key) -> Node[Value, Key]:
+    if root is None:
+        raise ValueError("This key not exist")
+    if root.key > key:
+        root.left_children = _delete(root.left_children, key)
+        return root
+    elif root.key < key:
+        root.right_children = _delete(root.right_children, key)
+        return root
+    root = _create_new_root(root)
+    return root
+
+
+def _preorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
+    return filter(None, (node, node.left_children, node.right_children))
+
+
+def _inorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
+    return filter(None, (node.left_children, node, node.right_children))
+
+
+def _postorder_comparator(node: Node[Value, Key]) -> Iterable[Node[Value, Key]]:
+    return filter(None, (node.left_children, node.right_children, node))
+
+
+def traverse(map: Tree, order: str) -> list[Value]:
+    nodes = []
+
+    def traverse_recursion(cur_node: Node[Value, Key], order_func: Callable):
+        node_order = order_func(cur_node)
+        for node in node_order:
+            if node is not cur_node:
+                traverse_recursion(node, order_func)
+            else:
+                nodes.append(node.data)
+
+    match order:
+        case "pre-order":
+            traverse_recursion(map.root, _preorder_comparator)
+        case "in-order":
+            traverse_recursion(map.root, _inorder_comparator)
+        case "post-order":
+            traverse_recursion(map.root, _postorder_comparator)
+
+    return nodes
